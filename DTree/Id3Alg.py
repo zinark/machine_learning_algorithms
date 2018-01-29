56bb1ace6afb6b418943f0458bafb231070341ad
Z <ceferhat@gmail.com>
2018-01-30 00:46:31 +0300

    id3 decide tree

--- DTree/Id3Alg.py
+++ DTree/Id3Alg.py
@@ -6,18 +6,17 @@ class Id3Alg(object):
     def fit(self, data):
         self.data = pd.DataFrame(data)
 
-    def entropy_from_data (self, data):
+    def entropy_from_data(self, data):
         freq = data.value_counts()
         values = freq.keys()
 
         possibilities = []
         for value in values:
-            #print value, freq[value], len(dt)
+            # print value, freq[value], len(dt)
             p = 1. * freq[value] / len(data)
             possibilities.append(p)
 
         return self.entropy(possibilities)
-
 
     def information_gain(self, target_attr, class_attr):
         dt = self.data
@@ -28,18 +27,18 @@ class Id3Alg(object):
 
         freq = dt[target_attr].value_counts()
         for k in freq.keys():
-            #print target_attr, "=", k, ", the distribution of", class_attr
+            # print target_attr, "=", k, ", the distribution of", class_attr
             filtered_dt = dt[dt[target_attr] == k]
             freq_inner = filtered_dt[class_attr].value_counts()
 
             plist = []
             for fi in freq_inner.keys():
                 p = 1. * freq_inner[fi] / len(filtered_dt)
-                #print "\t\t", fi, p
+                # print "\t\t", fi, p
                 plist.append(p)
             prob = 1. * len(filtered_dt) / len(dt[target_attr])
             entropy = self.entropy(plist)
-            #print "plist =", entropy, prob
+            # print "plist =", entropy, prob
             result += entropy * prob
 
         return base_entropy - result
@@ -54,7 +53,6 @@ class Id3Alg(object):
         tree = {}
         dt = self.data
 
-
         gained_infos = {}
         for attr in dt.columns.values:
             if attr == class_attr: continue
@@ -64,13 +62,13 @@ class Id3Alg(object):
         max_attr = max(gained_infos)
         print max_attr, gained_infos[max_attr]
 
-
         for val in dt[max_attr].value_counts().keys():
             print val
             if not tree.has_key(max_attr):
                 tree[max_attr] = {}
             tree[max_attr][val] = {}
 
+        to_iterate_attr = []
         for val in tree[max_attr]:
             print val
             data = dt[dt[max_attr] == val][class_attr]
@@ -78,5 +76,8 @@ class Id3Alg(object):
             e = self.entropy_from_data(data)
             if e == 0:
                 tree[max_attr][val] = data.iloc[0]
+            else:
+                to_iterate_attr.append(val)
 
+        print to_iterate_attr
         print tree
